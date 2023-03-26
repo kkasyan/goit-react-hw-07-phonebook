7cb2afbff9e0544a7ab3c767eb886d5f3a2473a6
kkasyan <104075234+kkasyan@users.noreply.github.com>
2023-03-26 11:50:16 +0300

    deleted chakra UI for a while to make it in the next task

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './components/shared/theme';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ChakraProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
